8a78775a15f38e07eb3eac682e5d48313c06114c
Joel Wong <joelwongjy@gmail.com>
2025-04-27 23:13:06 +0800

    Initial Commit

---
+++ scripture memory/scripture_memoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  scripture_memoryApp.swift
+//  scripture memory
+//
+//  Created by Joel Wong on 27/4/25.
+//
+
+import SwiftUI
+
+@main
+struct scripture_memoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
